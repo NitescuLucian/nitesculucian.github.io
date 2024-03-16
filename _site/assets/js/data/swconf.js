9f6d49d9e63d40bbb0d6d65de840b0cd97ea66fd
NitescuLucian <nitesculuci@gmail.com>
2024-03-16 15:05:22 +0200

    remove comments

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710593457',resources: [
+    cacheName: 'chirpy-1710594264',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
