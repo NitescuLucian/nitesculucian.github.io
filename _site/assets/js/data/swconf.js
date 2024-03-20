f3c7acf1067b67caf0a9cfa90a79f82920fb9b22
NitescuLucian <nitesculuci@gmail.com>
2024-03-21 00:33:04 +0200

    added consent

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710626860',resources: [
+    cacheName: 'chirpy-1710973888',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
